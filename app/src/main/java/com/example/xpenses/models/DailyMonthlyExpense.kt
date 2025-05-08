3a0df97042aa6db1f79f8994a8a67f4d9a770a1a
Dmayank297 <139100813+Dmayank297@users.noreply.github.com>
2025-05-08 15:52:54 +0530

    Update files

---
+++ app/src/main/java/com/example/xpenses/models/DailyMonthlyExpense.kt
@@ -0,0 +1,11 @@
+package com.example.xpenses.models
+
+data class DailyExpense(
+    val dayOfWeek: String, // 0 (Sunday) to 6 (Saturday)
+    val total: Double
+)
+
+data class MonthlyExpense(
+    val month: String, // 01 (Jan) to 12 (Dec)
+    val total: Double
+)
